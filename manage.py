5d19d3d3530f7b414b8c8eb3033e93369a69442b
Nick Berejan <njberejan@gmail.com>
2016-05-18 10:21:15 -0400

    settings and manage stuff

--- manage.py
+++ manage.py
@@ -6,6 +6,9 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE",
                           "questionbox_project.settings")
 
+#error making migrations, ask cam tomorrow.
+
     from django.core.management import execute_from_command_line
+
 
     execute_from_command_line(sys.argv)
