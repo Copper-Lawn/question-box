664f2789d2931576ae674f32bc32b7e6c1dbdbb1
oakes <oakesonline@gmail.com>
2016-05-20 15:33:35 -0400

    Registration progress

--- questionbox_project/urls.py
+++ questionbox_project/urls.py
@@ -6,14 +6,15 @@ from stackunderflow import views
 router = routers.DefaultRouter()
 router.register(r'api/questions', views.QuestionsViewSet)
 router.register(r'api/answers', views.AnswersViewSet)
+# router.register(r'account/create/$', views.CreateAccountView)
 
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^home/$', views.HomeView.as_view()),
-    url(r'^question/(?P<pk>[0-9]+)/$', views.QuestionDetailView.as_view()),
-    url(r'^account/create/$', views.CreateAccountView.as_view()),
-    url(r'^account/profile/(?P<pk>[0-9]+)/$', views.ProfileView.as_view()),
+    # url(r'^home/$', views.HomeView.as_view()),
+    # url(r'^question/(?P<pk>[0-9]+)/$', views.QuestionDetailView.as_view()),
+    url(r'^account/create/$', views.CreateAccountView.as_view(), name='create_account'),
+    # url(r'^account/profile/(?P<pk>[0-9]+)/$', views.ProfileView.as_view()),
     url(r'^admin/', include(admin.site.urls)),
 
 ]
--- stackunderflow/views.py
+++ stackunderflow/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from .models import Question, Answer
 from .serializers import QuestionSerializer, AnswerSerializer
+from django.views.generic.base import TemplateView
+from django.contrib.auth.models import User
 
 
 class QuestionsViewSet(viewsets.ModelViewSet):
@@ -17,3 +19,41 @@ class AnswersViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(creator=self.request.user)
+
+
+class CreateAccountView(TemplateView):
+    template_name = "stackunderflow/register.html"
+    # queryset = User.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['users_list'] = User.objects.all()
+        return context
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def register(request):
+#     if request.method == 'POST':
+#         user_form = UserCreationForm(request.POST)
+#         if user_form.is_valid():
+#             new_user = user_form.save()
+#             return HttpResponseRedirect("/board/")
+#         else:
+#             print(user_form.errors)
+#     else:
+#         user_form = UserCreationForm()
+#
+#     context = {'user_form': user_form}
+#     return render(request, "registration/register.html", context)
