cbfc44c04c5769978764caad99b410768f8dd997
Nick Berejan <njberejan@gmail.com>
2016-05-20 16:12:21 -0400

    done

--- stackunderflow/views.py
+++ stackunderflow/views.py
@@ -1,6 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions, renderers
 from .models import Question, Answer
 from .serializers import QuestionSerializer, AnswerSerializer
+from django.views.generic.base import TemplateView
 
 
 class QuestionsViewSet(viewsets.ModelViewSet):
@@ -9,6 +10,26 @@ class QuestionsViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(creator=self.request.user)
+
+class QuestionsPageView(TemplateView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    renderer_classes = (renderers.TemplateHTMLRenderer,)
+    template_name = 'stackunderflow/question.html'
+
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['given_question'] = Question.objects.filter(id=kwargs['pk'])
+        return context
+
+    # def get_context_data(self, **kwargs):
+    #     context = super(QuestionsPageView, self).get_context_data(**kwargs)
+    #     context['questions_list'] = Question.objects.all()
+    #     return context
 
 
 class AnswersViewSet(viewsets.ModelViewSet):
